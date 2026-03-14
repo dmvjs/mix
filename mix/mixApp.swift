574099d154db83d0bb64ac7f0d5aadf6c750c92b
elliott <elliott@ionq.co>
2026-03-14 18:43:17 -0400

    Initial Commit

---
+++ mix/mixApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixApp.swift
+//  mix
+//
+//  Created by Kirk Elliott on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct mixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
